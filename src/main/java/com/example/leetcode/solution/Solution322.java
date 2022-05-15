dbc0bf4be45ba9297c8b29e5feefa3d011a10280
Always <784682065@qq.com>
2022-05-15 16:47:52 +0800

    字符串相乘, 注意进位,翻转数组就好了

--- src/main/java/com/example/leetcode/solution/Solution322.java
+++ src/main/java/com/example/leetcode/solution/Solution322.java
@@ -11,9 +11,9 @@ import java.util.Arrays;
 public class Solution322 {
 
     public static void main(String[] args) {
-        int[] coins = {1, 2, 5};
+        int[] coins = { 2, 5};
 
-        System.out.println(coinChange(coins, 11));
+        System.out.println(coinChange(coins, 1));
     }
 
     public static int coinChange(int[] coins, int amount) {
@@ -37,4 +37,34 @@ public class Solution322 {
 
         return dp[amount] > amount ? -1 : dp[amount];
     }
+
+
+    public static int coinChange_1(int[] coins, int amount) {
+        if (amount < 1) {
+            return 0;
+        }
+        return coinChange(coins, amount, new int[amount]);
+    }
+
+    private static int coinChange(int[] coins, int rem, int[] count) {
+        if (rem < 0) {
+            return -1;
+        }
+        if (rem == 0) {
+            return 0;
+        }
+        if (count[rem - 1] != 0) {
+            return count[rem - 1];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, rem - coin, count);
+            if (res >= 0 && res < min) {
+                min = 1 + res;
+            }
+        }
+        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return count[rem - 1];
+    }
+
 }
